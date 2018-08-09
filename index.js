bd50d8b279ff9bd5514a2985f5c7ff95988ad96a
Jess Guttenberg <jessigu@gmail.com>
2018-08-09 17:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(number) {
   var array = [];
-  number = 0
-  
+  number = 0;
+  do {
+    array.push('I love the Beatles!');
+    number++;
+  } while (number < 15);
+  return array;
 }
