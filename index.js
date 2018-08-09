bb751b12ed5d04adec4d2f141b052426c218a6cc
Jess Guttenberg <jessigu@gmail.com>
2018-08-09 16:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function theBeatlesPlay(musician, instrument) {
   return empty;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  
+}
