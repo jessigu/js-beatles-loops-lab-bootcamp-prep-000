df782a260151b35214949efa9e75cffa7a90e357
Jess Guttenberg <jessigu@gmail.com>
2018-08-09 17:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 const musician = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
 const instrument = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
 
-function
+function theBeatlesPlay(musician, instrument) {
+  empty = [];
+  for (let i = 0; i < musician.length; i++) {
+    empty.push(musician[i] + ' plays ' + instrument[i]);
+  }
+  return empty;
+}
 
 const facts = [
   "He was the last Beatle to learn to drive",
